ca22e8350853c4295cd430de488dbf8cc45c3f5d
IMCHOUDHARY <36573566+IMCHOUDHARY@users.noreply.github.com>
2016-04-09 06:12:06 +0530

    first hello

---
+++ array4.java
@@ -0,0 +1,12 @@
+class Arr4{
+public static void main(String []args)
+{
+    int first[]=new int []{1,2,3,4,5,6,7,8};
+    int i=0;
+    int sum=0;
+    for(i=0;i<7;i++)
+    {
+        if(first[i]<5) System.out.println("Value:- "+first[i]);
+    }
+}
+}
